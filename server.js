c7505098ab609b753a4a70de9480ebd3f158798f
viniciusz0411 <viniciuszandomenic@gmail.com>
2026-06-02 20:48:18 -0300

    Semi Completo.
    Added body-parser and express middleware for JSON and static files. Established SQLite database connection and created 'clientes' table with routes for saving and listing clients.

---
+++ server.js
@@ -0,0 +1,47 @@
+const express=require('express');
+const sqlite3=require('sqlite3').verbose();
+const bodyParser=require('body-parser');
+const app=express();
+
+//Configuração do servidor.
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());//Necessário para o carrinho de compras.
+app.use(express.static('.'));//Serve seus arquivoss HTML, JS e CSS.
+
+//Conexão com o banco de dados.
+const db=new sqlite3.Database('./sissenai.db');
+
+//Inicialização de tabelas.
+db.serialize(()=>{
+  //Tabela de clientes.
+  db.run(`CREATE TABLE IF NOT EXISTS clientes(
+    id INTEGER PRIMARY JEY AUTOINCREMENT,
+    nome TEXT, 
+    cpf TEXT,
+    telefone TEXT
+    )`);
+
+//--ROTA DE CLIENTES--
+app.post('/salvar-cliente',(req,res)=>{
+  const{ nome,cpf,telefone}=req.body;
+  db.run(`INSERT INTO clientes (nome, cpf, telefone) VALUES(?,?,?)`, [nome,cpf,telefone], (err)=>{
+    if(err) return res.status(500).send(err.message);
+    res.redirect('/clientes.html');
+  });
+});
+
+app.get('/listar-clientes',(req,res)=>{
+  db.all("SELECT*FROM clientes",[],(err,rows)=>{
+    if(err)return res.status(500).json(err);
+    res.json(rows);
+  });
+});
+
+//Iniciar servidor
+const PORT=3000;
+app.listen(PORT,()=>{
+  console.log('=================================================='
+  console.log(`SISSENAI DISPONÍVEL EM: https://localhost:${PORT}`)   
+  console.log('=================================================='
+});
+
